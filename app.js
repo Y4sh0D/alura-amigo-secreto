283bf926973cafaf21e67c87583351989d98d18c
Diego Caballero <caballero11ct@gmail.com>
2025-08-17 16:25:03 -0500

    Creación de la función agregarNombre

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let arrayDeAmigosBase = [];
+
+function agregarNombre(nombre){
+    nombre = document.getElementById("amigo");
+    if (nombre.value === "") {
+        alert("¡El nombre no puede estar vacío!");
+        return;
+    }
+    nombre = nombre.value.trim();
+    arrayDeAmigosBase.push(nombre);
+    alert("¡El nombre ha sido agregado correctamente!");
+    
+    document.getElementById("amigo").value = "";
+    console.log(arrayDeAmigosBase);
+}
+
+
